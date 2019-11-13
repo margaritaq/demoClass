b8d900835b802d60095cc47edf2a06817c15d9a9
Margarita <margarita.gabureac@endava.com>
2019-11-13 15:18:52 +0200

    Java Basic Class First Presentation

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,10 +1,12 @@
 public class MainClass {
+
     public static void main(String[] args) {
-Animal suny = new Animal();
-suny.setAge(2);
-suny.setName("Flafy");
-        System.out.println("Animal Name: " + suny.getName() + " "+" Age: "+ suny.getAge());
-        Animal bugi = new Animal("Bob",5);
-        System.out.println("Pet name: " + bugi.getName() +" Age: "+ bugi.getAge());
+
+        Animal firstAnimal = new Animal();
+        firstAnimal.setAge(2);
+        firstAnimal.setName("Flaffy");
+        System.out.println("Animal Name: " + firstAnimal.getName() + "   Age: " + firstAnimal.getAge());
+        Animal secondAnimal = new Animal("Bob", 5);
+        System.out.println("Pet name: " + secondAnimal.getName() + "  Age: " + secondAnimal.getAge());
     }
 }
